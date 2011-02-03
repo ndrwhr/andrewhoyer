484c0ab91098c348aab52982e815f32fc9efdc30
Andrew Hoyer <me@andrew-hoyer.com>
2011-02-02 21:39:30 -0800

    Making the giant switch over to Sass

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "resources/css"
+sass_dir = "resources/scss"
+images_dir = "resources/images"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
