b53d0d4ee38d119dbcb5a8078bde041385365ee1
Andrew Hoyer <me@andrew-hoyer.com>
2011-02-11 00:34:48 -0800

    Decked out fancy minimal layout context menu

--- resources/js/minimal.js
+++ resources/js/minimal.js
@@ -1,7 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', function(){
+    // Color links
     var colors = ['blue', 'orange', 'green', 'red', 'teal', 'purple', 'yellow'];
     Array.prototype.slice.call(document.querySelectorAll('a')).forEach(function(link){
         link.className += ' ' + colors[Math.floor(Math.random() * colors.length)];
     });
+    
+    // Add more/less functionality to context menu.
+    var context = document.querySelector('.context');
+    if(context){
+        var control = context.querySelector('a.context-control');
+        
+        control.addEventListener('click', function(){
+            var text = control.textContent.toLowerCase();
+            
+            if (text == 'less...'){
+                context.className += ' less';
+                control.innerHTML = 'more...';
+            } else {
+                context.className = context.className.replace('less', '');
+                control.innerHTML = 'less...';
+            }
+        });
+    }
 });
