8508fb665787db7ad3cc8c27066abb79f7a91d91
Andrew Hoyer <me@andrew-hoyer.com>
2014-03-13 21:48:00 -0700

    Remove blob build step from the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@
 set -e
 
 rm -rvf _site &&
-    (cd experiments/blob; ./build.sh; ./making-of/build.py) &&
-    jekyll build &&
+    jekyll build
     rsync -atvz --delete _site/ asquared@andrew-hoyer.com:~/public_html/
